50f0aa5c42b6abbad2e40c0d5a5cbc0960c863af
JawadSher <jawadsher04@gmail.com>
2026-05-17 12:09:51 +0500

    Initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,6 @@
 import { Stack } from "expo-router";
+// @ts-ignore: side-effect import of CSS handled by Expo Router
+import "../global.css";
 
 export default function RootLayout() {
   return <Stack />;
